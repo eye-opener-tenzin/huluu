ddbbface1a9cfd669e679d95bbb9d204f987f2c4
Zyanvel <zyanvel@gmail.com>
2021-07-09 18:21:53 -0700

    setup repository for huluu app

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import Header from '../components/Header';
+
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Welcome to Huluu</title>
+      </Head>
+      <h1>Hello Huluu :)</h1>
+      <Header/>
+
+      {/*Navbar*/}
+      
+      {/*Results*/}
+    </div>
+  )
+}
