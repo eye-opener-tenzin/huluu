d43c88806af373670cd16bffc736cdcf44999266
spagnon7 <66347458+spagnon7@users.noreply.github.com>
2021-07-26 18:45:10 -0700

    added all heroIcons and container div

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import {
+  HomeIcon,
+  LightningBoltIcon,
+  BadgeCheckIcon,
+  CollectionIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 export default function Header() {
-    return (
-        <header>
-            <h1>huluu</h1>
-        </header>
-    )
+  return (
+    <header>
+      <div className="flex">
+        <HomeIcon className="h-10 w-10" />
+        <LightningBoltIcon className="h-10 w-10" />
+        <BadgeCheckIcon className="h-10 w-10" />
+        <CollectionIcon className="h-10 w-10" />
+        <SearchIcon className="h-10 w-10" />
+        <UserIcon className="h-10 w-10" />
+      </div>
+    </header>
+  );
 }
